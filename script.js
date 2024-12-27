b202d34f797778ed2ba14ebb67b0b707160edc87
Rais Zainuri <35108804+raiszainuri@users.noreply.github.com>
2024-12-27 08:52:13 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,280 @@
+const alumniData = [
+    {
+        "nickname": "Adrian",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Ahmad",
+        "campus": "Universitas Amikom Yogyakarta",
+        "ig": "https://www.instagram.com/ahmadaldiyanto/",
+        "linkedin": "https://www.linkedin.com/in/ahmad-aldiyanto-87b657330/",
+        "photo": "https://lh3.googleusercontent.com/d/1673O6owkZHG7pTr_CTqdU_imuSalepfd"
+    },
+    {
+        "nickname": "Akbar",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Alvin",
+        "campus": "Universitas Bunda Mulia",
+        "ig": "https://www.instagram.com/alvxnh?igsh=MnJzYTZyNGx2emwz",
+        "linkedin": "https://www.linkedin.com/in/alvinhartono/",
+        "photo": "https://lh3.googleusercontent.com/d/1SfLBzYZeMT2IRjuTtJezRl--tJFfMZlB"
+    },
+    {
+        "nickname": "Candra",
+        "campus": "Universitas Negeri Surabaya",
+        "ig": "https://instagram.com/cbar_bagus",
+        "linkedin": "www.linkedin.com/in/candra-bagus-ainur-rochman",
+        "photo": "https://lh3.googleusercontent.com/d/1NrMN8vVGGR3WXCw_vS7k0gSZlp9blUJD"
+    },
+    {
+        "nickname": "Dewi",
+        "campus": "Universitas Pelita Harapan",
+        "ig": "https://www.instagram.com/dewipuspita______",
+        "linkedin": "https://www.linkedin.com/in/dewi-puspita-241517272/",
+        "photo": ""
+    },
+    {
+        "nickname": "Eko",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Falih",
+        "campus": "Institut Digital Ekonomi LPKIA",
+        "ig": "https://www.instagram.com/17frnaufr/",
+        "linkedin": "https://linkedin.com/in/falih-naufal-254a80192",
+        "photo": ""
+    },
+    {
+        "nickname": "Fisan",
+        "campus": "Universitas Amikom Yogyakarta",
+        "ig": "https://www.instagram.com/f_i_s_a_n/",
+        "linkedin": "https://www.linkedin.com/in/fisan-syafa-nayoma-b250082a9?trk=contact-info",
+        "photo": "https://lh3.googleusercontent.com/d/1C7bhAOCIgJpgFYauxbmjM4Bib2XbLtqw"
+    },
+    {
+        "nickname": "Tanto",
+        "campus": "Sekolah Tinggi Teknologi Terpadu Nurul Fikri",
+        "ig": "https://www.instagram.com/tnto.ajah",
+        "linkedin": "https://www.linkedin.com/in/tantomukti/",
+        "photo": "https://lh3.googleusercontent.com/d/1eeoSVf35wIwx3z5bJccX22u037IBpKOq"
+    },
+    {
+        "nickname": "Melda",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Anin",
+        "campus": "Politeknik Negeri Jakarta",
+        "ig": "https://www.instagram.com/kasanindit/",
+        "linkedin": "https://www.linkedin.com/in/kasamira-anindita-qairia-9aa88524b/",
+        "photo": ""
+    },
+    {
+        "nickname": "Hasfi",
+        "campus": "Universitas Riau",
+        "ig": "https://www.instagram.com/hasfi.ayd/",
+        "linkedin": "https://www.linkedin.com/in/hashfiayd/",
+        "photo": "https://lh3.googleusercontent.com/d/1K-rCyvrGdBST5ocft41j_WBkPYjBc-nc"
+    },
+    {
+        "nickname": "Hamzah",
+        "campus": "Universitas Jember",
+        "ig": "https://www.instagram.com/amirull.hamzah/",
+        "linkedin": "https://www.linkedin.com/in/moh-amirul-hamzah/",
+        "photo": "https://lh3.googleusercontent.com/d/1uwFonJ9cyDcb3s2xtkr1JFYi61-39PTg"
+    },
+    {
+        "nickname": "Ardi",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Jalil",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Rizki",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Narapati",
+        "campus": "Universitas Pasundan",
+        "ig": "www.instagram.com/lio_keysa24/",
+        "linkedin": "https://www.linkedin.com/in/narapati-keysa/",
+        "photo": "https://lh3.googleusercontent.com/d/1jSdjX-H5uGwX_Z-5dlXksU2zdOZRW3Im"
+    },
+    {
+        "nickname": "Danang",
+        "campus": "Universitas Singaperbangsa Karawang",
+        "ig": "https://www.instagram.com/prdnwhyu_",
+        "linkedin": "www.linkedin.com/in/pradana-wahyu-hidayanto",
+        "photo": "https://lh3.googleusercontent.com/d/1db1Y55n54t8LAIw51rg36FRZkmOQ8V9c"
+    },
+    {
+        "nickname": "Raja",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Reza",
+        "campus": "Universitas PGRI Semarang",
+        "ig": "https://www.instagram.com/__rezaadityaa/",
+        "linkedin": "https://linkedin.com/in/rezaaditya04/",
+        "photo": ""
+    },
+    {
+        "nickname": "Ryan",
+        "campus": "Universitas Teknologi Bandung",
+        "ig": "https://www.instagram.com/ryan.rizaldy99/",
+        "linkedin": "www.linkedin.com/in/ryan-rizaldy-a00164251",
+        "photo": "https://lh3.googleusercontent.com/d/1Sr0K5forzHoIJ7SKU8BBBCATx4PLAOvb"
+    },
+    {
+        "nickname": "Tika",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Abel",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": ""
+    },
+    {
+        "nickname": "Yunus",
+        "campus": "Universitas Negeri Surabaya",
+        "ig": "https://www.instagram.com/yunsdh_",
+        "linkedin": "https://www.linkedin.com/in/yunus-dhanzky-h-a90521294/",
+        "photo": "https://lh3.googleusercontent.com/d/1C4dM0O4Rhg460wE01Iura3jHkxw8pnPf"
+    }
+];
+
+const randomImages = [
+    "https://picsum.photos/200/300?random=1",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=3",
+    "https://picsum.photos/200/300?random=4",
+    "https://picsum.photos/200/300?random=5",
+    "https://picsum.photos/200/300?random=6",
+    "https://picsum.photos/200/300?random=7",
+    "https://picsum.photos/200/300?random=8",
+    "https://picsum.photos/200/300?random=9",
+    "https://picsum.photos/200/300?random=10",
+    "https://picsum.photos/200/300?random=11",
+    "https://picsum.photos/200/300?random=12",
+    "https://picsum.photos/200/300?random=13",
+    "https://picsum.photos/200/300?random=14",
+    "https://picsum.photos/200/300?random=15",
+    "https://picsum.photos/200/300?random=16",
+    "https://picsum.photos/200/300?random=17",
+    "https://picsum.photos/200/300?random=18",
+    "https://picsum.photos/200/300?random=19",
+    "https://picsum.photos/200/300?random=20",
+    "https://picsum.photos/200/300?random=21",
+    "https://picsum.photos/200/300?random=22",
+    "https://picsum.photos/200/300?random=23",
+    "https://picsum.photos/200/300?random=24",
+    "https://picsum.photos/200/300?random=25"
+];
+
+
+function createAlumniCard(alumni) {
+    const col = document.createElement('div');
+    col.className = 'col-md-4 col-lg-3';
+
+    const card = document.createElement('div');
+    card.className = 'alumni-card';
+
+    const img = document.createElement('img');
+    if (alumni.photo === "") {img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/340px-Default_pfp.svg.png";}
+    else {img.src = alumni.photo;}
+    img.alt = `${alumni.nickname} photo`;
+    
+    const name = document.createElement('h2');
+    name.textContent = alumni.nickname;
+    
+    const campus = document.createElement('p');
+    campus.textContent = alumni.campus;
+    
+    const links = document.createElement('div');
+    links.className = 'links';
+    
+    const igLink = document.createElement('a');
+    igLink.href = alumni.ig;
+    igLink.textContent = 'Instagram';
+    igLink.target = '_blank';
+    
+    const separator = document.createElement('span');
+    separator.textContent = '  ';
+
+    const linkedinLink = document.createElement('a');
+    linkedinLink.href = alumni.linkedin;
+    linkedinLink.textContent = 'LinkedIn';
+    linkedinLink.target = '_blank';
+
+    links.appendChild(igLink);
+    links.appendChild(separator);
+    links.appendChild(linkedinLink);
+    
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(campus);
+    card.appendChild(links);
+    
+    col.appendChild(card);
+    return col;
+}
+
+function assignRandomImages(alumniData, images) {
+    return alumniData.map(alumni => {
+        return {
+            ...alumni,
+            // photo: images[Math.floor(Math.random() * images.length)]
+        };
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const alumniContainer = document.getElementById('alumniContainer');
+
+    const alumniWithRandomImages = assignRandomImages(alumniData, randomImages);
+    const shuffledAlumniData = alumniWithRandomImages.sort(() => Math.random() - 0.5);
+
+    shuffledAlumniData.forEach(alumni => {
+        const col = createAlumniCard(alumni);
+        alumniContainer.appendChild(col);
+    });
+
+    new Masonry('#alumniContainer', {
+        itemSelector: '.col-md-4',
+        columnWidth: '.col-md-4',
+        percentPosition: true
+    });
+});
